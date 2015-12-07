6a9aa5eafd4a936cc4ece750cb88a3d3344bb274
= <tsmit291@icloud.com>
2015-12-07 10:22:56 -0700

    foxes are pringing wahoo

--- scripts/index.js
+++ scripts/index.js
@@ -8,4 +8,21 @@
 // added and displayed.  Example:  a new add button would change all
 // to display: block in the existing dom and add a new random fox which is
 // also display: block.  Hit the display float button and they all change
-// back to float and it adds a new floated random fox. 
+// back to float and it adds a new floated random fox.
+// var foxy = collection[images][1]["img_src"];
+//
+// $(document).ready(function(){
+//   $("#trigger").click(function(){
+//     $("#container").append("collection"["images"][1]["img_src"])
+//   for (var foxy = 0; i < foxy.length; i++) {
+//     console.log("foxy");
+//   }
+// });
+//
+// });
+
+
+$("#trigger").click(function(){
+  var randomNum = Math.floor((Math.random() *3));
+  $("#container").append('<img src="'+collection["images"][randomNum]["image_src"]+'"/>');
+});
