ca49668e915e2b62d8891eb27cf4f87d575dee60
Nmuta Jones <nmuta@Nmutas-MacBook-Pro.local>
2015-12-07 08:59:51 -0700

    add files

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+//instructions:
+
+// Write a script that , when you click on the button,
+// adds a new fox RANDOM image to the container div.
+// All foxes should be floated.  Make another button to remove all foxes.
+
+// Stretch:   Make several buttons that change the way the foxes are
+// added and displayed.  Example:  a new add button would change all
+// to display: block in the existing dom and add a new random fox which is
+// also display: block.  Hit the display float button and they all change
+// back to float and it adds a new floated random fox. 
---
+++ scripts/resources.js
@@ -0,0 +1,19 @@
+var collection =
+{
+	"scope": "Western USA",
+	"genre": "gaming",
+	"images": [{
+			"name": "renault",
+			"image_src": "images/fox1.png"
+		}, {
+			"name": "renee",
+			"image_src": "images/fox2.png"
+		}, {
+			"name": "renai",
+			"image_src": "images/fox3.png"
+		}
+
+	],
+	"domain": "public"
+
+}
